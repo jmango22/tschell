a3f293e8014a12bf6d4731ce34c2b69cff8b345c
jmango22 <jonathonmeng@gmail.com>
2017-10-05 14:17:13 -0600

    first commit

---
+++ shell/cmake-build-debug/CMakeFiles/shell.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell.dir/myint.c.o"
+  "CMakeFiles/shell.dir/myspin.c.o"
+  "CMakeFiles/shell.dir/mysplit.c.o"
+  "CMakeFiles/shell.dir/mystop.c.o"
+  "CMakeFiles/shell.dir/tsh.c.o"
+  "shell.pdb"
+  "shell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
